4cc7f073c3e138d086ffdc6f52a1cca6e7d4d7d6
Artem Chuban <ArtemChuban@yandex.ru>
2026-08-17 11:03:09 +0200

    Roow window name change

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use x11rb::connection::Connection;
+use x11rb::protocol::xproto::{AtomEnum, PropMode};
+use x11rb::wrapper::ConnectionExt as _;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (connection, screen_num) = x11rb::connect(None)?;
+    let screen = &connection.setup().roots[screen_num];
+    let root = screen.root;
+
+    let text = "bslstatus: hello";
+
+    connection.change_property8(
+        PropMode::REPLACE,
+        root,
+        AtomEnum::WM_NAME,
+        AtomEnum::STRING,
+        text.as_bytes(),
+    )?;
+    connection.flush()?;
+
+    println!("Root window name set to: {text}");
+
+    Ok(())
 }
